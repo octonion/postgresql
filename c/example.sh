5506480607638b98ee3e24a6e394deebf9591826
Christopher D. Long <galizur@gmail.com>
2018-01-02 23:41:29 -0500

    Need sudo

--- c/example.sh
+++ c/example.sh
@@ -6,7 +6,7 @@ gcc -fPIC -I`pg_config --includedir-server` -c funcs.c
 gcc -shared -o funcs.so funcs.o
 
 sudo mkdir /usr/local/lib/postgresql/
-cp funcs.so /usr/local/lib/postgresql/
+sudo cp funcs.so /usr/local/lib/postgresql/
 
 psql test -f funcs.sql
 psql test -f check.sql
